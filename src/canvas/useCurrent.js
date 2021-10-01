ab8b9509467a43db757bc8b2d279e51a77d99382
Lil-El <yxd99324@qq.com>
2021-10-01 19:40:28 +0800

    update useCurrent eventName

--- src/canvas/useCurrent.js
+++ src/canvas/useCurrent.js
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react"
 
+// Tips: useCurrent在多个hook及页面中调用，会触发多次事件
 export default (canvas)=>{
-    const [current, setCurrent] = useState(null)
+    const [current, setCurrent] = useState(null); // 当前选中的，可能是object，也可能是group
+    const [selected, setSelected] = useState([]); // 当前选中的所有object
 
     useEffect(()=>{
-        canvas && canvas.on("mouse:down", ({target})=>{
-            setCurrent(target)
-        })
+        if(canvas) {
+            // canvas.on("mouse:down", ({target})=>{
+            //     setCurrent(target)
+            // })
+            canvas.on("selection:created", ({selected, target})=>{
+                setCurrent(target);
+                setSelected(selected);
+            })
+            canvas.on("selection:updated", ({selected, target})=>{
+                setCurrent(target);
+                setSelected(selected);
+            })
+            canvas.on("selection:cleared", ()=>{
+                setCurrent(null);
+                setSelected([]);
+            })
+        }
     }, [canvas])
 
-    return [current]
+    return [current, selected]
 }
